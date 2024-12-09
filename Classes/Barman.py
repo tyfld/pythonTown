b717d0381d980046fe7412acfcfb456981d1c9ae
tyfld <tyfenn.ledevehat@gmail.com>
2024-12-09 22:54:55 +0100

    Barman

---
+++ Classes/Barman.py
@@ -0,0 +1,17 @@
+from .Humain import Humain
+
+class Barman(Humain):
+    def __init__(self, nom, nom_bar=None, boisson_pref="vin"):
+        super().__init__(nom, boisson_pref)
+        self.__nom_bar = nom_bar
+
+    def se_presenter(self):
+        super().se_presenter()
+        self.parle(f"Bienvenue dans mon bar : Chez '{self.__nom_bar}', coco.")
+
+    def quelEstTonNom(self):
+        return super().quelEstTonNom()
+    
+    def servir(self, humain):
+        if isinstance(humain, Humain):
+            self.parle(f"Tenez {humain.quelEstTonNom()}, voici un verre de {humain.quelleEstTaBoissonPref()}, coco.")
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from Classes.Brigand import Brigand
 from Classes.Cowboy import Cowboy
 from Classes.Dame import Dame
+from Classes.Barman import Barman
 
 if __name__ == "__main__":
     brigand = Brigand("Bill","tord-boyaux")
     cowboy = Cowboy("John", "whisky")
     dame = Dame("Daisy","rouge","lait")
+    barman = Barman("Tony", "Tony", "vin")
 
 
 #Notre scénario
@@ -21,3 +23,7 @@ cowboy.liberer(dame)
 print()
 brigand.se_presenter()
 cowboy.se_presenter()
+
+print()
+barman.se_presenter()
+barman.servir(cowboy)
